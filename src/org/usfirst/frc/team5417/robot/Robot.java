8c79c39ac8442751a6fb672b04d1ea64bbb75731
Bryan Taylor <btaylor@Bryans-MacBook-Pro.local>
2015-01-13 19:59:01 -0600

    Adding missing project and classpath files

--- src/org/usfirst/frc/team5417/robot/Robot.java
+++ src/org/usfirst/frc/team5417/robot/Robot.java
@@ -1,17 +1,15 @@
-
 package org.usfirst.frc.team5417.robot;
 
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
+ * This is a demo program showing the use of the RobotDrive class, specifically it 
+ * contains the code necessary to operate a robot with tank drive.
  *
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the SampleRobot
@@ -24,42 +22,30 @@ import edu.wpi.first.wpilibj.Timer;
  * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
 public class Robot extends SampleRobot {
-    RobotDrive myRobot;
-    Joystick stick;
-
+    RobotDrive myRobot;  // class that handles basic drive operations
+    XboxController controller;  // set to ID 1 in DriverStation
     public Robot() {
         myRobot = new RobotDrive(0, 1);
         myRobot.setExpiration(0.1);
-        stick = new Joystick(0);
+        controller = new XboxController(0);
     }
 
-    public void Dummy()
-    {
-    }
+    
     /**
-     * Drive left & right motors for 2 seconds then stop
-     */
-    public void autonomous() {
-        myRobot.setSafetyEnabled(false);
-        myRobot.drive(-0.5, 0.0);	// drive forwards half speed
-        Timer.delay(2.0);		//    for 2 seconds
-        myRobot.drive(0.0, 0.0);	// stop robot
-    }
-
-    /**
-     * Runs the motors with arcade steering.
+     * Runs the motors with tank steering.
      */
     public void operatorControl() {
         myRobot.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) {
-            myRobot.arcadeDrive(stick); // drive with arcade style (use right stick)
+        	if (false == isOperatorControl() && false == isEnabled())
+        	{
+        		int a = 23423942 & (0xFF << 8);
+        	}
+//        	myRobot.tankDrive(leftStick, rightStick);
+            myRobot.tankDrive(controller.getLY(Hand.kLeft), controller.getRY(Hand.kRight), true);
+        	
             Timer.delay(0.005);		// wait for a motor update time
         }
     }
 
-    /**
-     * Runs during test mode
-     */
-    public void test() {
-    }
 }

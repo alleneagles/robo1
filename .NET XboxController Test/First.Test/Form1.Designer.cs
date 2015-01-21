3a1327f3c928cfa5789498fd5d75b30da1d35532
dawsonjk <dawsonjk@gmail.com>
2015-01-20 20:41:07 -0600

    .NET XboxController test program

---
+++ .NET XboxController Test/First.Test/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace First.Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtL = new System.Windows.Forms.TextBox();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtT = new System.Windows.Forms.TextBox();
+            this.txtS = new System.Windows.Forms.TextBox();
+            this.txtF = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtL
+            // 
+            this.txtL.Location = new System.Drawing.Point(64, 122);
+            this.txtL.Name = "txtL";
+            this.txtL.Size = new System.Drawing.Size(100, 20);
+            this.txtL.TabIndex = 0;
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(469, 122);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(100, 20);
+            this.txtR.TabIndex = 1;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(266, 122);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "L:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "C:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(447, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "R:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(447, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "s:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(244, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "t:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "f:";
+            // 
+            // txtT
+            // 
+            this.txtT.Location = new System.Drawing.Point(266, 54);
+            this.txtT.Name = "txtT";
+            this.txtT.Size = new System.Drawing.Size(100, 20);
+            this.txtT.TabIndex = 8;
+            // 
+            // txtS
+            // 
+            this.txtS.Location = new System.Drawing.Point(469, 54);
+            this.txtS.Name = "txtS";
+            this.txtS.Size = new System.Drawing.Size(100, 20);
+            this.txtS.TabIndex = 7;
+            // 
+            // txtF
+            // 
+            this.txtF.Location = new System.Drawing.Point(64, 54);
+            this.txtF.Name = "txtF";
+            this.txtF.Size = new System.Drawing.Size(100, 20);
+            this.txtF.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 222);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtT);
+            this.Controls.Add(this.txtS);
+            this.Controls.Add(this.txtF);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.txtL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtL;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtT;
+        private System.Windows.Forms.TextBox txtS;
+        private System.Windows.Forms.TextBox txtF;
+    }
+}
+
---
+++ .NET XboxController Test/First.Test/Form1.cs
@@ -0,0 +1,121 @@
+﻿using J2i.Net.XInputWrapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace First.Test
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer _pollingTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            XboxController.StartPolling();
+
+            _pollingTimer = new System.Timers.Timer(25);
+            _pollingTimer.AutoReset = true;
+            _pollingTimer.Elapsed += _pollingTimer_Elapsed;
+            _pollingTimer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                XboxController.StopPolling();
+
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        void _pollingTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            double L, R, C, f, t, s;
+            CalcStrafeDrive(out L, out R, out C, out f, out t, out s);
+
+            this.Invoke(new Action(() =>
+            {
+                txtL.Text = string.Format("{0:F4}", L);
+                txtR.Text = string.Format("{0:F4}", R);
+                txtC.Text = string.Format("{0:F4}", C);
+                txtF.Text = string.Format("{0:F4}", f);
+                txtT.Text = string.Format("{0:F4}", t);
+                txtS.Text = string.Format("{0:F4}", s);
+            }));
+        }
+
+        private double SignificantDigits(double value, int digitCount)
+        {
+            double factor = Math.Pow(10, digitCount);
+            int trunc = (int)(factor * value);
+            return trunc / factor;
+        }
+
+        public void CalcStrafeDrive(out double L, out double R, out double C, out double f, out double t, out double s)
+        {
+            double Lfix = 1.0;
+            double Rfix = 1.0;
+            double Sfix = 1.0;
+
+            var controller = XboxController.RetrieveController(0);
+
+            f = controller.LeftThumbStick.Y / (double)short.MaxValue;
+            s = controller.LeftThumbStick.X / (double)short.MaxValue;
+            t = controller.RightThumbStick.X / (double)short.MaxValue;
+
+            f = SignificantDigits(f, 2);
+            s = SignificantDigits(s, 2);
+            t = SignificantDigits(t, 2);
+
+            double numerL = Math.Pow(f * 0.5, 2) + Math.Pow(t * 0.5, 2);
+            double numerR = Math.Pow(f * 0.5, 2) - Math.Pow(t * 0.5, 2);
+            double denom = Math.Pow(0.5, 2) + Math.Pow(0.5, 2);
+
+            L = (numerL / denom) * Lfix;
+            R = (numerR / denom) * Rfix;
+            C = Math.Pow(s, 2) * Sfix;
+
+            L = SignificantDigits(L, 2);
+            R = SignificantDigits(R, 2);
+            C = SignificantDigits(C, 2);
+
+            if (t < 0)
+            {
+                // swap L and R
+                double temp = L;
+                L = R;
+                R = temp;
+            }
+
+            // this should be greater than some minimum threshold because a very small negative value,
+            // even when the left stick is still, could cause the left and right motors to get messed up
+            if (f < -0.1)
+            {
+                // invert the signs for L and R
+                L = -L;
+                R = -R;
+            }
+
+            if (s < 0)
+            {
+                // invert the sign for C
+                C = -C;
+            }
+        }
+
+    }
+}
